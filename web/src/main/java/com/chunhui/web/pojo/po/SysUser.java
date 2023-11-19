94d55d7bf807488b2c2199a68566deab2e608dbe
genghui <chunhui128@outlook.com>
2023-11-20 00:57:46 +0800

    新增mapstruct

--- web/src/main/java/com/chunhui/web/pojo/po/SysUser.java
+++ web/src/main/java/com/chunhui/web/pojo/po/SysUser.java
@@ -2,12 +2,9 @@ package com.chunhui.web.pojo.po;
 
 import lombok.Data;
 
-import java.io.Serializable;
-
 @Data
-public class SysUser implements Serializable {
+public class SysUser extends BaseDO {
 
-    private int id;
     private String userName;
     private String password;
 }
--- web/src/main/java/com/chunhui/web/service/SysUserService.java
+++ web/src/main/java/com/chunhui/web/service/SysUserService.java
@@ -5,8 +5,10 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.chunhui.web.dao.SysUserDao;
 import com.chunhui.web.exception.BusinessException;
 import com.chunhui.web.exception.ExceptionEnum;
+import com.chunhui.web.mapstruct.CommonConvert;
 import com.chunhui.web.pojo.po.SysUser;
 import com.chunhui.web.pojo.vo.Result;
+import com.chunhui.web.pojo.vo.SysUserVO;
 import com.chunhui.web.util.ResultGeneretor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -27,6 +29,8 @@ public class SysUserService {
     @Resource
     private RedisTemplate redisTemplate;
 
+    private CommonConvert commonConvert;
+
     public String hello() {
         return sysUserDao.hello();
     }
@@ -35,7 +39,7 @@ public class SysUserService {
         return sysUserDao.getById("1");
     }
 
-    public SysUser getCurrentUser() throws Exception {
+    public SysUserVO getCurrentUser() throws Exception {
         HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
         String token = request.getHeader("token");
         if (StringUtils.isBlank(token)) {
@@ -45,7 +49,7 @@ public class SysUserService {
         if (null == user) {
             throw new BusinessException(ExceptionEnum.NO_LOGIN);
         }
-        return user;
+        return commonConvert.toOut(user);
     }
 
     public Result<String> login() {
