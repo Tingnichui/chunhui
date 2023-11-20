9451aa3d5e031e5be3fd59f3d7ea196c8be67911
genghui <chunhui128@outlook.com>
2023-11-20 21:27:27 +0800

    引入element plus

--- admin-web/src/main.js
+++ admin-web/src/main.js
@@ -1,6 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+let app = createApp(App);
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+app.mount('#app')
