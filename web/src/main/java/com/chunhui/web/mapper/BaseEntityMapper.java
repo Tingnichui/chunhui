7ef944a4cc0b8d0534e0b797147c7fb1a5b9b257
genghui <chunhui128@outlook.com>
2023-11-22 23:12:51 +0800

    更新gitignore

--- web/src/main/java/com/chunhui/web/mapper/BaseEntityMapper.java
+++
@@ -1,7 +0,0 @@
-package com.chunhui.web.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface BaseEntityMapper<T> extends BaseMapper<T> {
-
-}
--- web/src/main/java/com/chunhui/web/mapper/SysUserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.chunhui.web.mapper;
-
-import com.chunhui.web.pojo.po.SysUser;
-
-public interface SysUserMapper extends BaseEntityMapper<SysUser> {
-    String hello();
-
-}
