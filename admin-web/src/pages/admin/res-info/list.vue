98b08aacc2bb781a76c88d0e2fa73956e02339d3
genghui <chunhui128@outlook.com>
2024-01-04 10:43:03 +0800

    格式化代码

--- admin-web/src/pages/admin/res-info/list.vue
+++ admin-web/src/pages/admin/res-info/list.vue
@@ -2,10 +2,10 @@
   <div class="page">
     <el-card>
       <el-form
-          class="query-form"
-          inline
           :label-width="80"
           :model="searchForm"
+          class="query-form"
+          inline
       >
         <el-form-item
             label="标题 :"
@@ -26,8 +26,8 @@
           />
         </el-form-item>
         <div class="action-groups">
-          <el-button type="primary" plain @click="search">查询</el-button>
-          <el-button type="primary" plain @click="research">重置</el-button>
+          <el-button plain type="primary" @click="search">查询</el-button>
+          <el-button plain type="primary" @click="research">重置</el-button>
         </div>
       </el-form>
     </el-card>
@@ -36,34 +36,34 @@
         <div class="card-header">
           <el-space><span>菜单管理</span></el-space>
           <el-space>
-            <el-button type="primary" plain @click="showSaveForm">新增</el-button>
+            <el-button plain type="primary" @click="showSaveForm">新增</el-button>
           </el-space>
         </div>
       </template>
       <el-table
           :data="tableData.records"
-          style="width: 100%"
-          size="default"
-          height="500"
           :highlight-current-row="true"
-          row-key="id"
-          empty-text="No Data"
           :stripe="true"
+          empty-text="No Data"
+          height="500"
+          row-key="id"
+          size="default"
+          style="width: 100%"
       >
         <el-table-column
-            prop="title"
             label="标题"
+            prop="title"
         />
         <el-table-column
-            prop="content"
             label="内容"
+            prop="content"
         />
         <el-table-column
-            prop="act"
-            label="操作"
             :width="160"
             align="center"
             fixed="right"
+            label="操作"
+            prop="act"
         >
           <template #default="scope">
             <el-space>
@@ -76,18 +76,18 @@
     </el-card>
     <el-card>
       <el-pagination
-          layout="total,prev,pager,next,sizes"
+          v-model:current-page="tableData.current"
+          v-model:page-size="tableData.pageSize"
           :background="true"
+          :page-sizes="[15,30,50,80,100]"
           :small="true"
           :total="tableData.total"
-          :page-sizes="[15,30,50,80,100]"
-          v-model:page-size="tableData.pageSize"
-          v-model:current-page="tableData.current"
+          layout="total,prev,pager,next,sizes"
           @current-change="changeCurrentPage"
           @size-change="changePageSize"
       />
     </el-card>
-    <el-dialog v-model="saveDialogFlag" center :title="updateFlag ? '修改' : '新增'" width="40%">
+    <el-dialog v-model="saveDialogFlag" :title="updateFlag ? '修改' : '新增'" center width="40%">
       <el-form :model="saveForm" label-position="right" label-width="80px">
         <el-form-item label="标题">
           <el-input v-model="saveForm.title"/>
