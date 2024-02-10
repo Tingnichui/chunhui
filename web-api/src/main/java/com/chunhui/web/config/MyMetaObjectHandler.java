e0de5cf74a5c2a25ab2c3725dccfc74ec5bdf086
genghui <chunhui128@outlook.com>
2024-02-10 20:08:59 +0800

    修改mybatis-plus自动更新bug

--- web-api/src/main/java/com/chunhui/web/config/MyMetaObjectHandler.java
+++ web-api/src/main/java/com/chunhui/web/config/MyMetaObjectHandler.java
@@ -9,6 +9,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.reflection.MetaObject;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.Date;
 
@@ -26,12 +28,18 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         try {
             this.strictInsertFill(metaObject, CREATE_BY, String.class, ServerletUtil.getUserInfo().getId());
         } catch (Exception e) {
-            // 获取当前线程id
-            String processId = MDC.get("processId");
-            if (StringUtils.isBlank(processId)) {
-                throw new RuntimeException(ExceptionEnum.SAVE_FAIL.getMessage());
+            // 判断是不是web现成 1.是:填充线程id 2.不是就填充方法名
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (null != attributes) {
+                String processId = MDC.get("processId");
+                if (StringUtils.isBlank(processId)) {
+                    throw new RuntimeException(ExceptionEnum.SAVE_FAIL.getMessage());
+                }
+                this.strictInsertFill(metaObject, CREATE_BY, String.class, processId);
+            } else {
+                this.strictInsertFill(metaObject, CREATE_BY, String.class, "task");
             }
-            this.strictInsertFill(metaObject, CREATE_BY, String.class, processId);
+
         }
         // 自动填充创建时间
         this.strictInsertFill(metaObject, CREATE_TIME, Date.class, new Date());
@@ -43,12 +51,19 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
         try {
             this.strictUpdateFill(metaObject, UPDATE_BY, String.class, ServerletUtil.getUserInfo().getId());
         } catch (Exception e) {
-            // 获取当前线程id
-            String processId = MDC.get("processId");
-            if (StringUtils.isBlank(processId)) {
-                throw new RuntimeException(ExceptionEnum.SAVE_FAIL.getMessage());
+            // 判断是不是web现成 1.是:填充线程id 2.不是就填充方法名
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (null != attributes) {
+                // 获取当前线程id
+                String processId = MDC.get("processId");
+                if (StringUtils.isBlank(processId)) {
+                    throw new RuntimeException(ExceptionEnum.SAVE_FAIL.getMessage());
+                }
+                this.strictUpdateFill(metaObject, UPDATE_BY, String.class, processId);
+            } else {
+                this.strictUpdateFill(metaObject, UPDATE_BY, String.class, "task");
             }
-            this.strictUpdateFill(metaObject, CREATE_BY, String.class, processId);
+
         }
         // 自动填充创建时间
         this.strictUpdateFill(metaObject, UPDATE_TIME, Date.class, new Date());
